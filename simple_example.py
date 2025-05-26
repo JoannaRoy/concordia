41232504c624d6be37e88245c80846ed4a0844d0
Joanna Roy <62259358+JoannaRoy@users.noreply.github.com>
2025-05-26 22:31:29 +0200

    add simple example file

---
+++ simple_example.py
@@ -0,0 +1,102 @@
+import numpy as np
+from IPython import display
+import sentence_transformers
+from concordia.language_model import gpt_model
+from concordia.language_model import no_language_model
+from concordia.prefabs.simulation import generic as simulation
+import concordia.prefabs.entity as entity_prefabs
+import concordia.prefabs.game_master as game_master_prefabs
+from concordia.typing import prefab as prefab_lib
+from concordia.utils import helper_functions
+
+def setup_language_model(api_key, model_name, disable_model=False):
+    if not api_key and not disable_model:
+        raise ValueError('GPT_API_KEY is required.')
+
+    if not disable_model:
+        return gpt_model.GptLanguageModel(api_key=api_key, model_name=model_name)
+    return no_language_model.NoLanguageModel()
+
+def setup_embedder(disable_model=False):
+    if disable_model:
+        return np.ones(3)
+
+    st_model = sentence_transformers.SentenceTransformer('sentence-transformers/all-mpnet-base-v2')
+    return lambda x: st_model.encode(x, show_progress_bar=False)
+
+def get_prefabs():
+    return {
+        **helper_functions.get_package_classes(entity_prefabs),
+        **helper_functions.get_package_classes(game_master_prefabs),
+    }
+
+def get_instances():
+    return [
+        prefab_lib.InstanceConfig(
+            prefab='basic__Entity',
+            role=prefab_lib.Role.ENTITY,
+            params={
+                'name': 'Oliver Cromwell',
+                'goal': 'become lord protector',
+            },
+        ),
+        prefab_lib.InstanceConfig(
+            prefab='basic__Entity',
+            role=prefab_lib.Role.ENTITY,
+            params={
+                'name': 'King Charles I',
+                'goal': 'avoid execution for treason',
+            },
+        ),
+        prefab_lib.InstanceConfig(
+            prefab='generic__GameMaster',
+            role=prefab_lib.Role.GAME_MASTER,
+            params={
+                'name': 'default rules',
+                'extra_event_resolution_steps': '',
+            },
+        ),
+        prefab_lib.InstanceConfig(
+            prefab='formative_memories_initializer__GameMaster',
+            role=prefab_lib.Role.INITIALIZER,
+            params={
+                'name': 'initial setup rules',
+                'next_game_master_name': 'default rules',
+                'shared_memories': [
+                    'The king was captured by Parliamentary forces in 1646.',
+                    'Charles I was tried for treason and found guilty.',
+                ],
+            },
+        ),
+    ]
+
+def create_simulation_config():
+    return prefab_lib.Config(
+        default_premise='Today is January 29, 1649.',
+        default_max_steps=5,
+        prefabs=get_prefabs(),
+        instances=get_instances(),
+    )
+
+def run_simulation(model, embedder, config):
+    runnable_simulation = simulation.Simulation(
+        config=config,
+        model=model,
+        embedder=embedder,
+    )
+    return runnable_simulation()
+
+def main():
+    GPT_API_KEY = ''
+    GPT_MODEL_NAME = 'gpt-4.1-nano'
+    DISABLE_LANGUAGE_MODEL = False
+
+    model = setup_language_model(GPT_API_KEY, GPT_MODEL_NAME, DISABLE_LANGUAGE_MODEL)
+    embedder = setup_embedder(DISABLE_LANGUAGE_MODEL)
+    config = create_simulation_config()
+
+    results_log = run_simulation(model, embedder, config)
+    display.HTML(results_log)
+
+if __name__ == '__main__':
+    main()
