11b2a199ef8ddb1146143ec6ae74f340ef2479d1
Joanna Roy <62259358+JoannaRoy@users.noreply.github.com>
2025-06-09 10:44:51 +0200

    update remote env script

--- bin/setup_remote_env.sh
+++ bin/setup_remote_env.sh
@@ -1,6 +1,11 @@
 rm -rf .venv
 
-pt update
+ssh-keygen -t ed25519 -C "runpod@runpod.io" -f ~/.ssh/id_ed25519 -N ""
+cat ~/.ssh/id_ed25519.pub | pbcopy
+echo "Please paste the SSH key into GitHub and press Enter to continue..."
+read
+
+apt update
 apt install -y software-properties-common
 add-apt-repository ppa:deadsnakes/ppa
 apt update
